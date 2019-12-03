9da9c379200e2f1870d6aa5ca3a55a8549770838
20191844303 <56478672+20191844303@users.noreply.github.com>
2019-12-03 09:56:57 +0800

    Add files via upload

---
+++ c7/7 9/7 9.cpp
@@ -0,0 +1,37 @@
+// 7 9.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<stdio.h>
+int letter,digit,space,others;
+int main(int argc, char* argv[])
+{
+	void count(char str[]);
+	char text[80];
+	printf("input string:\n");
+		gets(text);
+	printf("string");
+	puts(text);
+	letter=0;
+	digit=0;
+	space=0;
+	others=0;
+	count(text);
+	printf("\nletter:%d\ndigit:%d\nspace:%d\nothers:%d\n",letter,digit,space,others);
+
+	return 0;
+}
+
+void count(char str[])
+{
+	int i;
+	for(i=0;str[i]!='\0';i++)
+		if((str[i]>='a'&&str[i]<'z')||(str[i]>='A'&&str[i]<='Z'))
+			letter++;
+		else if(str[i]>='0'&&str[i]<='9')
+			digit++;
+		else if(str[i]==32)
+			space++;
+		else 
+			others++;
+}
